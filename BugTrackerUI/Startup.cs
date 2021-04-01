1844ee80c93e0367eeb7835f0f85e8883602cb17
pigullino1975 <44727413+pigullino1975@users.noreply.github.com>
2021-04-01 17:27:50 -0300

    Register the BugService for Dependency Injection

--- BugTrackerUI/Startup.cs
+++ BugTrackerUI/Startup.cs
@@ -26,6 +26,8 @@ namespace BugTrackerUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IBugService, BugService>();
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
         }
