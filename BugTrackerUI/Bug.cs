677a48d5d63ee2221539aebe33c1d012afa57dd8
pigullino1975 <44727413+pigullino1975@users.noreply.github.com>
2021-04-01 17:40:45 -0300

    Add the Required Attributes to the Model

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -9,11 +9,11 @@ namespace BugTrackerUI
     public class Bug
     {
         public int Id { get; set; }
-
+        [Required]
         public string Title { get; set; }
-
+        [Required]
         public string Description { get; set; }
-
+        [Required]
         public int Priority { get; set; }
     }
 }
